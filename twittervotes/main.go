2f8ef555e310b4276572316846c0d848f64f30fa
Vineet Daniel <vineetdaniel@gmail.com>
2016-10-21 01:06:38 +0530

    a

--- twittervotes/main.go
+++ twittervotes/main.go
@@ -1,3 +1,99 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+
+	nsq "github.com/bitly/go-nsq"
+
+	mgo "gopkg.in/mgo.v2"
+)
+
+var db *mgo.Session
+
+type poll struct {
+	Options []string
+}
+
+func main() {
+	if err := dialdb(); err != nil {
+		log.Fatalln("failed to dial Mongodb", err)
+	}
+	defer closedb()
+	var stoplock sync.Mutex
+	stop := false
+	stopchan := make(chan struct{}, 1)
+	signalChan := make(chan os.Signal, 1)
+	go func() {
+		<-signalChan
+		stoplock.Lock()
+		stop = true
+		stoplock.Unlock()
+		log.Println("stopping")
+		stopchan <- struct{}{}
+		closeConn()
+	}()
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+
+	votes := make(chan string)
+	publisherStoppedChan := publishVotes(votes)
+	twitterStoppedChan := startTwitterStream(stopchan, votes)
+	go func() {
+		for {
+			time.Sleep(1 * time.Minute)
+			closeConn()
+			stoplock.Lock()
+			if stop {
+				stoplock.Unlock()
+				break
+			}
+			stoplock.Unlock()
+		}
+	}()
+	<-twitterStoppedChan
+	close(votes)
+	<-publisherStoppedChan
+
+}
+
+func dialdb() error {
+	var err error
+	log.Println("dialling mongodb")
+	db, err = mgo.Dial("localhost")
+	return err
+}
+
+func closedb() {
+	db.Close()
+	log.Println("closed db connection")
+}
+
+func loadOptions() ([]string, error) {
+	var options []string
+	iter := db.DB("ballots").C("polls").Find(nil).Iter()
+	var p poll
+	for iter.Next(&p) {
+		options = append(options, p.Options...)
+	}
+	iter.Close()
+	return options, iter.Err()
+}
+
+func publishVotes(votes <-chan string) <-chan struct{} {
+	stopchan := make(chan struct{}, 1)
+	pub, _ := nsq.NewProducer("localhost:4160", nsq.NewConfig())
+	go func() {
+		for vote := range votes {
+			pub.Publish("votes", []byte(vote))
+		}
+		log.Println("Publisher: Stopping")
+		pub.Stop()
+		log.Println("Publisher: Stopped")
+		stopchan <- struct{}{}
+	}()
+	return stopchan
+}
--- twittervotes/twitter.go
+++ twittervotes/twitter.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"io"
 	"log"
 	"net"
 	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
 	"sync"
 	"time"
 
@@ -19,8 +23,12 @@ var (
 )
 var (
 	authSetupOnce sync.Once
-	httpClient    http.Client
+	httpClient    *http.Client
 )
+
+type tweet struct {
+	Text string
+}
 
 func dial(netw, addr string) (net.Conn, error) {
 	if conn != nil {
@@ -62,9 +70,96 @@ func setupTwitterAuth() {
 		Secret: ts.AccessSecret,
 	}
 	authClient = &oauth.Client{
-		Credentials: &oauth.Credentials{
+		Credentials: oauth.Credentials{
 			Token:  ts.ConsumerKey,
 			Secret: ts.ConsumerSecret,
 		},
 	}
+}
+
+func makeRequest(req *http.Request, params url.Values) (*http.Response, error) {
+	authSetupOnce.Do(func() {
+		setupTwitterAuth()
+		httpClient = &http.Client{
+			Transport: &http.Transport{
+				Dial: dial,
+			},
+		}
+	})
+	//log.Println(creds)
+	formEnc := params.Encode()
+	req.Header.Set("Content-Type", "applicaton/x-www-form-urlencoded")
+	req.Header.Set("Content-Length", strconv.Itoa(len(formEnc)))
+	req.Header.Set("Authorization", authClient.AuthorizationHeader(creds, "POST", req.URL, params))
+	return httpClient.Do(req)
+}
+
+func readFromTwitter(votes chan<- string) {
+	options, err := loadOptions()
+	log.Println(options)
+	if err != nil {
+		log.Println("failed to load options", err)
+		return
+	}
+	u, err := url.Parse("https://stream.twitter.com/1.1/statuses/filter.json")
+	if err != nil {
+		log.Println("creating filter request failed", err)
+		return
+	}
+	query := make(url.Values)
+	query.Set("track", strings.Join(options, ","))
+	req, err := http.NewRequest("POST", u.String(), strings.NewReader(query.Encode()))
+	//log.Println(req)
+	if err != nil {
+		log.Println("creating filter request failed", err)
+		return
+	}
+	resp, err := makeRequest(req, query)
+	log.Println(resp)
+	if err != nil {
+		log.Println("making request failed", err)
+		return
+	}
+	reader := resp.Body
+	decoder := json.NewDecoder(reader)
+	for {
+		var tweet tweet
+		if err := decoder.Decode(&tweet); err != nil {
+			break
+		}
+		for _, option := range options {
+			if strings.Contains(
+				strings.ToLower(tweet.Text),
+				strings.ToLower(option),
+			) {
+				log.Println("vote", option)
+				votes <- option
+			}
+		}
+
+	}
+
+}
+
+func startTwitterStream(stopchan <-chan struct{}, votes chan<- string) <-chan struct{} {
+	stoppedchan := make(chan struct{}, 1)
+	go func() {
+		defer func() {
+			stoppedchan <- struct{}{}
+
+		}()
+		for {
+			select {
+			case <-stopchan:
+				log.Println("stopping twitter")
+				return
+			default:
+				log.Println("Querying Twitter")
+				readFromTwitter(votes)
+				log.Println(" (waiting)")
+				time.Sleep(10 * time.Second)
+			}
+		}
+	}()
+	return stoppedchan
 }
